e15d47652e1515ee3776c9a2f8fe28b15723a231
Benitto_raj <benittoraj@mallow-tech.com>
2022-05-03 14:03:08 +0530

    Components Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,25 @@
 export default {
     template: `
-                 <button class="btn btn-primary bg-gray-200 hover:bg-gray-500 border rounded px-5 py-3 mx-2 disabled:cursor-not-allowed" :disabled="processing">
+                 <button :class='{
+                "btn border rounded px-5 py-3 mx-2" : true,
+                   "bg-blue-200 hover:bg-blue-500 " : type=== "primary",
+                   "bg-green-200 hover:bg-green-500 " : type=== "success",
+                   "bg-red-200 hover:bg-red-500 " : type === "danger",
+                   "bg-grey-200 hover:bg-grey-500 " : type === "muted",
+                   "is-loading" : processing
+                 }'
+                 disabled="processing">
                     <slot/>
                 </button>
                 `,
-    data() {
-        return {
-            processing: true
-        };
-    }
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
+        }
+    },
 }
